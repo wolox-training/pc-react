f930ba4ddff9d345c5ad352bec1586bd27f86820
pabloccid <pablo.cid@wolox.com.ar>
2017-12-29 13:57:45 -0300

    Fixed PR #10 commentaries

--- react/src/components/BookDetailData/index.js
+++ react/src/components/BookDetailData/index.js
@@ -12,13 +12,16 @@ class BookDetailData extends Component {
       <div className="book-detail-data">
         <BookImage image_url={book.image_url} title={book.title} width={200} height={275} />
         <div className="book-detail-subdata">
-          <div className="book-detail-text">
+          <div className="book-detail-text-group">
             <h2 className="book-detail-title">{book.title}</h2>
             <h3 className="book-detail-subtitle">{book.author}</h3>
+          </div>
+          <div className="book-detail-text-group">
             <h3 className="book-detail-subtitle">{book.year}</h3>
             <h3 className="book-detail-subtitle">{book.genre}</h3>
-            <p className = "book-detail-summary">{book.summary}</p>
           </div>
+          <p className = "book-detail-summary">{book.summary}</p>
+
           <button className="button-rent">{strings.rent}</button>
         </div>
       </div>
