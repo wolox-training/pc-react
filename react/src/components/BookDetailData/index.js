6f89b26e6d6b81b613cc80ede3fa3cd1d6db3f59
pabloccid <pablo.cid@wolox.com.ar>
2017-12-29 13:11:07 -0300

    Fixed title styles

--- react/src/components/BookDetailData/index.js
+++ react/src/components/BookDetailData/index.js
@@ -13,11 +13,11 @@ class BookDetailData extends Component {
         <BookImage image_url={book.image_url} title={book.title} width={200} height={275} />
         <div className="book-detail-subdata">
           <div className="book-detail-text">
-            <h2 className="book-title">{book.title}</h2>
-            <h3 className="book-subtitle">{book.author}</h3>
-            <h3 className="book-subtitle">{book.year}</h3>
-            <h3 className="book-subtitle">{book.genre}</h3>
-            <p className = "book-summary">{book.summary}</p>
+            <h2 className="book-detail-title">{book.title}</h2>
+            <h3 className="book-detail-subtitle">{book.author}</h3>
+            <h3 className="book-detail-subtitle">{book.year}</h3>
+            <h3 className="book-detail-subtitle">{book.genre}</h3>
+            <p className = "book-detail-summary">{book.summary}</p>
           </div>
           <button className="button-rent">{strings.rent}</button>
         </div>
